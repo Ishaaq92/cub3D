a0343ea0702de7f80f20020543603385e5adcc55
ishaaq <a.ishaaq@icloud.com>
2025-09-10 18:52:38 +0100

    good

--- srcs/cub3D.c
+++ srcs/cub3D.c
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 12:40:08 by isahmed           #+#    #+#             */
-/*   Updated: 2025/09/10 16:23:18 by isahmed          ###   ########.fr       */
+/*   Updated: 2025/09/10 18:49:22 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 int main(int ac, char **av)
 {
 	t_data data;
-	// t_map config;
 
-	// data.config = config;
 	if (ac > 2)
 		return (1);
 	if (av[1] && parser(&data, av[1]))
@@ -25,11 +23,11 @@ int main(int ac, char **av)
 	for (int i=0; i < data.map.map_size; i++)
 		printf("- %s", data.map.map[i]);
 
-	// if (initialise_data(&data) == -1)
-	// 		exit(1);
-	// render(&data);
-	// mlx_hook(data.win, DestroyNotify, NoEventMask, ft_quit, &data);
-	// mlx_key_hook(data.win, key_hook, &data);
-	// mlx_loop(data.mlx);
-	// ft_quit(&data);
+	if (initialise_data(&data) == -1)
+			exit(1);
+	render(&data);
+	mlx_hook(data.win, DestroyNotify, NoEventMask, ft_quit, &data);
+	mlx_key_hook(data.win, key_hook, &data);
+	mlx_loop(data.mlx);
+	ft_quit(&data);
 }
