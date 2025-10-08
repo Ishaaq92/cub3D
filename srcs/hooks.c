d9df925379914317c380984e00a71caa8c79b72a
ishaaq <a.ishaaq@icloud.com>
2025-10-08 16:27:52 +0100

    wall detection should be working now. :

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: isahmed <isahmed@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 16:11:05 by isahmed           #+#    #+#             */
-/*   Updated: 2025/10/08 16:17:15 by isahmed          ###   ########.fr       */
+/*   Updated: 2025/10/08 16:26:30 by isahmed          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,23 @@ void	rotate(t_data *data, double rot_speed)
     double oldPlaneX = data->game->plane_x;
     data->game->plane_x = data->game->plane_x * cos(-rot_speed) - data->game->plane_y * sin(-rot_speed);
     data->game->plane_y = oldPlaneX * sin(-rot_speed) + data->game->plane_y * cos(-rot_speed);
+}
+
+int	wall_detection(t_data *data, double new_x, double new_y)
+{
+	double	gap;
+
+	gap = 0.3;
+	if (data->map.map[(int)(new_y)][(int)(new_x - gap)] == '1')
+		return (1);
+	else if (data->map.map[(int)(new_y)][(int)(new_x + gap)] == '1')
+		return (1);
+	else if (data->map.map[(int)(new_y - gap)][(int)(new_x)] == '1')
+		return (1);
+	else if (data->map.map[(int)(new_y + gap)][(int)(new_x)] == '1')
+		return (1);
+	else 
+		return(0);
 }
 
 void	walk(t_data *data, char dir, double scale)
@@ -38,8 +55,8 @@ void	walk(t_data *data, char dir, double scale)
 		new_x = data->game->player->x + (data->game->plane_x * scale);
 		new_y = data->game->player->y + (data->game->plane_y * scale);
 	}
-	// if (data->map.map[(int)(fabs(new_y - 0.3))][(int)(fabs(new_x - 0.3))] == '1')
-	// 	return ;
+	if (wall_detection(data, new_x, new_y) == 1)
+		return ;
 	data->game->player->x = new_x;
 	data->game->player->y = new_y;
 }
@@ -57,9 +74,9 @@ int	key_hook(int keycode, t_data *data)
 	else if (keycode == XK_d || keycode == XK_D)
 	 	walk(data, 'l', 0.1);
 	else if (keycode == XK_Right)
-		rotate(data, 0.3);
+		rotate(data, 0.1);
 	else if (keycode == XK_Left)
-		rotate(data, -0.3);
+		rotate(data, -0.1);
 	else
 		return (1);
 	// render(data);
